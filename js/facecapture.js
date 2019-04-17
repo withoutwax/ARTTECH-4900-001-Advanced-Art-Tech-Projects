d8c54f209099c909afe3b73e19eb9d69abe96106
Will <rlagmlckd@gmail.com>
2019-04-17 02:13:08 -0500

    Configuring methods to save images to implement newly updated user's face onto the live video

--- js/facecapture.js
+++ js/facecapture.js
@@ -9,49 +9,95 @@ Webcam.attach( '#my_camera' );
 // id_number variable has to be on global 
 // to generate unique id for newly captured user
 var id_number = 0;
+var id = "person_" + id_number;
 
 function take_snapshot() {
     // take snapshot and get image data
     Webcam.snap( function(data_uri) {
-        // display results in page
-        
         // downloading the image
         // download(data_uri, "person.jpg", "image/jpg");
         
         // previewing the image
         // console.log(data_uri);
         // window.open(data_uri);
+
+        // Display results in page
+        var capturedImageElement = document.querySelector('#results').getContext('2d');
+        var img = new Image();
+        img.onload = function() {
+            capturedImageElement.drawImage(img, 0, 0, 800, 600);
+        };
+        img.src = data_uri;
         
         // ************** Newly added captured users **************
-        // Todo -
+        // TODO:
         // Need to append the newly captured user to images lists
-        var id = "person_" + id_number;
-        var path = "./media/";
         images.push({"id":id, "path":data_uri});
-        // Update the id number
-        id_number += 1;
+        console.log(images);
+
+
 
         // ************** Track the captured image **************
-        var ntracker = new clm.tracker();
-        ntracker.init();
+        console.log(ntracker);
+        animateClean();
 
-        var capturedImageElement = document.querySelector('#results');
-        ntracker.start(capturedImageElement);
-
-        var newImagePositions = ntracker.getCurrentPosition();
-        var drawCanvas = document.querySelector('#results_sample_canvas');
-        ntracker.draw(drawCanvas);
-        console.log(newImagePositions);
-
-        // Storing / downloading the image
-        localStorage.setItem(data_uri, id);
-        // console.log(localStorage);
-        localStorage.clear();
-            
-        // console.log(images);
-
-        document.getElementById('results').innerHTML = 
-            '<h2>Here is your image:</h2>' + 
-            '<img src="'+data_uri+'" download/>';
+        // Update the id number
+        id_number += 1;
+        
     } );
+
+    
 }
+
+
+
+// ************** Track the captured image **************
+var ntracker = new clm.tracker({stopOnConvergence : true});
+ntracker.init();
+
+// ntracker.start(capturedImageElement);
+
+var resultsOverlay = document.querySelector('#results_overlay');
+console.log(resultsOverlay);
+var resultsOverlayCC = resultsOverlay.getContext('2d');
+
+var newImagePositions = ntracker.getCurrentPosition();
+// ntracker.draw(resultsOverlay);
+
+
+// Storing / downloading the image
+// localStorage.setItem(data_uri, id);
+// console.log(localStorage);
+// localStorage.clear();
+    
+// console.log(images);
+
+function animateClean() {
+    ntracker.start(document.querySelector('#results'));
+    drawLoop();
+}
+
+function animate(box) {
+    ntracker.start(document.querySelector('#results'));
+    drawLoop();
+}
+
+function drawLoop() {
+    drawRequest = requestAnimationFrame(drawLoop);
+    resultsOverlayCC.clearRect(0, 0, 800, 600);
+    if (ntracker.getCurrentPosition()) {
+        ntracker.draw(resultsOverlay);
+    }
+
+}
+
+document.addEventListener("clmtrackrConverged", function(event) {
+    document.querySelector('#captured_complete').innerHTML = "CAPTURED!";
+
+    cancelAnimationFrame(drawRequest);
+
+    // Need to save the coordinate for the user
+    console.log(ntracker.getCurrentPosition());
+    masks[id] = ntracker.getCurrentPosition();
+    console.log(masks);
+}, false);
